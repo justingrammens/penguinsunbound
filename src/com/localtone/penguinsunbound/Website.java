f12f56126cb6ba2b3419cac9a6309280c11191ca
Justin Grammens <justin@localtone.com>
2009-06-27 13:53:28 -0500

    Intial commit of code from presentation

---
+++ src/com/localtone/penguinsunbound/Website.java
@@ -0,0 +1,29 @@
+/**
+ * Created by Justin Grammens
+ * Copyright Localtone Interactive. All Rights Reserved.
+ */
+
+package com.localtone.penguinsunbound;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class Website extends Activity {
+
+	WebView webView;
+
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.website);
+
+		String link = this.getIntent().getStringExtra("link");
+			
+		webView = (WebView) findViewById(R.id.web_view);
+		webView.getSettings().setJavaScriptEnabled(true);
+		webView.loadUrl(link);
+
+	}
+
+}
